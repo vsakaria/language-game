b61e239eab255c49332e4f1e213b39dfca93d601
Vishal Sakaria <vsakaria@gmail.com>
2014-05-26 22:28:12 +0100

    Animation classes added to answer buttons

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,7 +3,8 @@
 var app  = angular
   .module('languageGameApp', [
     'ngRoute',
-    'ngResource'
+    'ngResource',
+    'ngAnimate'
   ])
 .config(function($routeProvider, $locationProvider) {
     $routeProvider
@@ -25,4 +26,36 @@ var app  = angular
     $locationProvider.html5Mode(false);
   });
 
+app.directive(['clicked', function(clicked) {
+  var controller = function($scope) {
+    $scope.showAnswer = false;
+  };
 
+  return {
+    restrict: 'C',
+    scope: false,
+    controller: controller
+  };
+}]);
+
+app.animation('.answer-animation', function(){
+  return {
+    beforeAddClass: function(element, className, done){
+      if (className === 'clicked') {
+        $(element).addClass('animated wobble');
+
+      }
+      else {
+        done();
+      }
+    },
+
+    beforeRemoveClass: function(element, className, done) {
+      if (className == 'clicked') {
+        }
+      else {
+        done();
+      }
+    }
+  };
+});
